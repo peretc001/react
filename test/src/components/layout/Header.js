d3398bfcdddef7e6c8133d8405a23bee1fc7d978
Igor Krasovsky <peretc001@mail.ru>
2021-06-05 14:11:08 +0300

    test смена кнопки в шапке

--- test/src/components/layout/Header.js
+++ test/src/components/layout/Header.js
@@ -1,18 +1,30 @@
 import logo from '../../assets/logo.svg'
 import Button from "../common/Button";
-import {Link} from 'react-router-dom'
+import {useLocation,Link} from 'react-router-dom'
 
 function Header() {
+    const location = useLocation();
+
     return (
         <header className="header">
             <Link to="/" className="logo">
                 <img src={logo} alt="LiveDune"/>
             </Link>
 
-            <p className="signup">У вас нет аккаунта?</p>
-            <Button url="/signup" name="Регистрация" type="btn-accent"/>
+            {!location.pathname.includes('signup')
+                ?
+                <>
+                    <p className="signup">У вас нет аккаунта?</p>
+                    <Button url="/signup" name="Регистрация" type="btn-accent"/>
+                </>
+                :
+                <>
+                    <p className="signup">Уже есть аккаунт?</p>
+                    <Button url="/" name="Войти" type="btn-accent"/>
+                </>
+            }
         </header>
-    );
+    )
 }
 
 export default Header;
